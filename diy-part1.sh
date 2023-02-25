c596d0281c2d3f2f5b108d4337706d433c02c3be
gundao <haluantuo@gmail.com>
2023-02-25 13:42:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-cd package && git clone https://github.com/kenzok8/small-package
-cd small-package && git clone https://github.com/vpei/luci-app-zerotier.git && git clone https://github.com/cokebar/openwrt-vlmcsd.git && git clone https://github.com/cokebar/luci-app-vlmcsd.git
+#cd package && git clone https://github.com/kenzok8/small-package
+#cd small-package && git clone https://github.com/vpei/luci-app-zerotier.git && git clone https://github.com/cokebar/openwrt-vlmcsd.git && git clone https://github.com/cokebar/luci-app-vlmcsd.git
